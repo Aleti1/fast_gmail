6758f77d64a1784c55a550573bb2c6f0e8c0a5cc
Aleti9 <alexealexandrudaniel@gmail.com>
2024-04-13 19:17:03 +0300

    ready for pypi test env

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("fast_gmail/README.md", "r") as f:
+    long_descrition=f.read()
+
 setup(
     name="fast-gmail",
     version="1.0.0",
-    description="Complete gmail api wrapper",
+    description="GmailApi wrapper",
+    long_description=long_descrition,  # Load description from README
+    long_description_content_type='text/markdown',
     author="Aleti",
     author_email="aleti.open.source@gmail.com",
     url="https://github.com/aleti1/fast-gmail",
     license="MIT",
     requires=[],
-    packages=find_packages(
-        exclude=("tests*", "testing*")
-    )
+    package_dir={"":"fast_gmail"},
+    packages=find_packages(where="fast_gmail"),
+    python_requires=">=3.10"
 )
